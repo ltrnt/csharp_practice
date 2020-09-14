1aa615434ce38e14d65bac865c8be5452c37caff
Maxim Filippov <maxim.fil1905@yandex.ru>
2020-09-14 18:02:37 +0400

    01 practice is done

---
+++ csharp_practice/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace csharp_practice
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Practice01.Task8();
+        }
+    }
+}
