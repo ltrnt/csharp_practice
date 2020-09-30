510a51b1a1d790f15a63b894b7b40a772b77da0c
Maxim Filippov <maxim.fil1905@yandex.ru>
2020-09-30 22:18:32 +0400

    Senior 22 y.o. update of Practice 5, Task2_5

--- csharp_practice/Practice05.cs
+++ csharp_practice/Practice05.cs
@@ -1,140 +1,76 @@
 ﻿using System;
+using System.Threading;
+
 namespace csharp_practice
 {
     public class Practice05
     {
-        public Practice05()
+        // Finding the nearest number with the same sum of odd digits
+        public static int FindSameSumOfOddDigits(int number, int counter = 1, int sumOfOddDigits = -1)
         {
+            if (sumOfOddDigits < 0) sumOfOddDigits = SumOfOddDigits(number);
+
+            if (SumOfOddDigits(number + counter) == sumOfOddDigits) return number + counter;
+            if (SumOfOddDigits(number - counter) == sumOfOddDigits) return number - counter;
+
+            return FindSameSumOfOddDigits(number, ++counter, sumOfOddDigits);
         }
 
-        // TASK START __________________________________________________________
-
-        // Рекурсивный поиск ближайшего числа с суммой нечетных цифр равной А
-
-        public static int findNearest(int startNumber, int sum, int step = 1)
-        {
-            int numberP = startNumber + step;
-            int numberM = startNumber - step;
-
-            if (sumOfDigitsBy1(numberP) == sum) return numberP;
-            if (sumOfDigitsBy1(numberM) == sum) return numberM;
-
-            return findNearest(startNumber, sum, ++step);
-        }
-
-        // Проверка, является ли число простым.
-        public static bool checkSimpleNumber(int number)
+        public static bool IsPrimeNumber(int number)
         {
             for (int i = 2; i <= Math.Sqrt(number); i++)
-            {
                 if (number % i == 0)
-                {
                     return false;
-                }
-            }
 
-            return true;
+            return number != 0;
         }
 
-        // Нахождение суммы нечетных цифр числа
-        public static int sumOfDigitsBy1(int number)
+        public static int SumOfOddDigits(int number)
         {
             int sum = 0;
 
-            while (number != 0)
-            {
-                if ((number % 10) % 2 == 1)
-                {
-                    sum += number % 10;
-                }
-
-                number /= 10;
-            }
+            for (; number != 0; number /= 10)
+                if (number % 2 == 1) sum += number % 10;
 
             return sum;
         }
 
         public static void Task2_5()
         {
+            const int numberOfValues = 4;
+            int[] values = new int[numberOfValues]; // [0] = a, [1] = b, [2] = C, [3] = A
 
-            /*
-                Разработать метод, который для заданного натурального числа N возвращает сумму его нечетных цифр.
-                С помощью данного метода:
+            Console.Write("Enter a, b, C, A separated by space: ");
+            string[] enteredValues = Console.ReadLine().Split(' ');
 
-                a) для каждого целого числа на отрезке [a, b] вывести на экран сумму его нечетных цифр;
-                b) вывести на экран только те целые числа отрезка [a, b], у которых сумма нечетных цифр числа равна заданному значению С;
-                c) вывести на экран только те целые числа отрезка [a, b], у которых сумма нечетных цифр является простым числом.
-                d) для заданного числа А вывести на экран ближайшее следующее по отношению к нему число, сумма нечетных цифр которого равна сумме нечетных цифр числа А.
-             */
+            for (int i = 0; i < numberOfValues; i++) values[i] = Convert.ToInt32(enteredValues[i]);
 
-            Console.Write("Введите число а: ");
-            int a = Convert.ToInt32(Console.ReadLine());
+            int a = values[0], b = values[1], C = values[2], A = values[3];
 
-            Console.Write("Введите число b: ");
-            int b = Convert.ToInt32(Console.ReadLine());
+            if (a > b) a = Interlocked.Exchange(ref b, a);
 
-            Console.Write("Введите число C: ");
-            int c = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Введите число A: ");
-            int aBIG = Convert.ToInt32(Console.ReadLine());
-
-            // A
-            if(a > b)
-            {
-                int temp = b;
-                b = a;
-                a = temp;
-            }
-
-            for(int i = a; i <= b; i++)
-            {
-                Console.Write(i + ": " + sumOfDigitsBy1(i));
-                Console.Write(", ");
-            }
-
-            Console.WriteLine();
-            Console.WriteLine();
-
-            // B
+            // Solution 'A'
             for (int i = a; i <= b; i++)
-            {
-                int sum = sumOfDigitsBy1(i);
-                if (sum == c)
-                {
-                    Console.Write(i + ": " + sum);
-                    Console.Write(", ");
-                }
-            }
-            Console.WriteLine();
-            Console.WriteLine();
+                Console.Write(i + " - " + SumOfOddDigits(i) + "; ");
 
-            // C
+            Console.Write("\n\n");
+
+            // Solution 'B'
             for (int i = a; i <= b; i++)
-            {
-                int sum = sumOfDigitsBy1(i);
-                if (sum != 0 && checkSimpleNumber(sum))
-                {
-                    Console.Write(i + ": " + sum);
-                    Console.Write(", ");
-                }
-            }
-            Console.WriteLine();
-            Console.WriteLine();
+                if (SumOfOddDigits(i) == C)
+                    Console.Write(i + "; ");
 
-            // D
-            int sumA = sumOfDigitsBy1(aBIG);
+            Console.Write("\n\n");
 
-            Console.WriteLine(findNearest(aBIG, sumA));
+            // Solution 'C'
+            for (int i = a; i <= b; i++)
+                if (IsPrimeNumber(SumOfOddDigits(i)))
+                    Console.Write(i + "; ");
+
+            Console.Write("\n\n");
+
+            // Solution 'D'
+            Console.WriteLine(FindSameSumOfOddDigits(A));
         }
-
-        // TASK END ____________________________________________________________
-        
-
-        public static void Task3_5()
-        {
-
-        }
-
     }
 }
