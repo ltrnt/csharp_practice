2bc93372a78749bd62cad8dc8142854cc427529e
Maxim Filippov <maxim.fil1905@yandex.ru>
2020-10-22 19:28:13 +0400

    Practice 09 is done. (File inculuded)

---
+++ csharp_practice/Practice09.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace csharp_practice
+{
+    public class Practice09
+    {
+        public Practice09()
+        {
+        }
+
+        public static void Task2_20()
+        {
+            // Дан файл, компонентами которого являются символы.
+            // Переписать в новый файл все символы, за которыми в первом файле следует данная буква.
+            string path = "/Users/ltrnt/Projects/csharp_practice/csharp_practice";
+
+            char check_symbol = Convert.ToChar(Console.Read());
+
+            using (FileStream fileIn = new FileStream(path + "/file05_20_in", FileMode.Open, FileAccess.Read))
+            {
+                using (FileStream fileOut = new FileStream(path + "/file05_20_out", FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    int symbol = -1;
+                    int current_symbol = -1;
+                    while ((current_symbol = fileIn.ReadByte()) != -1)
+                    {
+                        if (symbol < 0)
+                        {
+                            continue;
+                        }
+                        else if ((char)current_symbol == check_symbol)
+                        {
+                            fileOut.WriteByte((byte)symbol);
+                        }
+
+                        symbol = current_symbol;
+                    }
+                }
+            }
+        }
+
+    }
+}
--- csharp_practice/Program.cs
+++ csharp_practice/Program.cs
@@ -7,7 +7,7 @@ namespace csharp_practice
         public static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Practice08.Task3_20();
+            Practice09.Task2_20();
         }
     }
 }
