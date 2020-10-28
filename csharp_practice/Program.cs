ce0aff0c0df2da8277a5adc790b40e18bc67fc6f
Maxim Filippov <maxim.fil1905@yandex.ru>
2020-10-28 21:05:48 +0400

    Practice 08. Task 3 is done

--- csharp_practice/Program.cs
+++ csharp_practice/Program.cs
@@ -7,7 +7,7 @@ namespace csharp_practice
         public static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            foxlyume.Practice07.Task6_5();
+            foxlyume.Practice08.Task3_5();
         }
     }
 }
