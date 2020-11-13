659d512f908f1340e5597c8b739b7a360a91254c
Maxim Filippov <maxim.fil1905@yandex.ru>
2020-11-14 01:29:21 +0400

    Rebuild project structure

--- csharp_practice/Program.cs
+++ csharp_practice/Program.cs
@@ -1,57 +1,12 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 
 namespace csharp_practice
 {
-    //class MainClass
-    //{
-    //    public static void Main(string[] args)
-    //    {
-    //        SortedList list = new SortedList();
-
-    //        list.Add("B", 2);
-    //        list.Add("A", 1);
-
-    //        list["D"] = 4;
-    //        list["C"] = 3;
-
-    //        foreach(DictionaryEntry entry in list)
-    //        {
-    //            Console.WriteLine("{0}\t{1}", entry.Key, entry.Value);
-    //        }
-
-    //        // Вывод:
-    //        // A   1
-    //        // B   2
-    //        // C   3
-    //        // D   4
-    //    }
-    //}
-
     class MainClass
     {
         public static void Main(string[] args)
         {
-            SortedList<String, Int32> list = new SortedList<String, Int32>();
-
-            list.Add("B", 2);
-            list.Add("A", 1);
-
-            list["D"] = 4;
-            list["C"] = 3;
-
-            foreach (KeyValuePair<String, Int32> entry in list)
-            {
-                Console.WriteLine("{0}\t{1}", entry.Key, entry.Value);
-            }
-
-            // Вывод:
-            // A   1
-            // B   2
-            // C   3
-            // D   4
+            Console.WriteLine("Its work!");
         }
     }
-
 }
